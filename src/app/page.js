766d7f63a3a377596d167f77914f415696ba148b
Talha-G <mtalhagujjar285@gmail.com>
2025-01-07 23:24:25 +0500

    Any thing

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="w-[100%] flex justify-center items-center p-4 bg-black text-white font-bold text-[24px]">
+    Hello!
+   </div>
+  );
+}
