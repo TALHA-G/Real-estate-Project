4c847c2e7bb6b3e8e8d3f9d12bc69aee8f5cbe12
Talha-G <mtalhagujjar285@gmail.com>
2025-01-08 01:11:09 +0500

    updated info

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,27 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="w-[100%] h-[100vh] flex justify-center items-center p-4 bg-black text-white font-bold text-[24px]">
+    <div className="w-[100%] h-[100vh] flex justify-center items-center p-4 bg text-white font-bold text-[24px]">
       <div className="p-2">
-        <h2>**************(Under Investigation)***************</h2>
-        <div className="text-[18px] font-semibold m-4 p-4 px-8 text-yellow-700 bg-blue-400 rounded">
+        <div>
+          <h2>&nbsp;&nbsp;&nbsp;&nbsp;****(Under Investigation)****</h2>
+        </div>
+        <div className="w-[100%] text-[20px] font-semibold my-4 p-8 text-red-700 bg-blue-400 rounded">
           <ol>
-            <li>Name : Alishba</li>
-            <li>Qualification: BS(Math)</li>
-            <li>Address City: Gujrawala</li>
-            <li>Date of Birth: 12 January</li>
-            <li>Eye color: Light brown</li>
-            <li>Hair color: Brown</li>
-            <li>Height: 5.2`</li>
-            <li>Weight: 50kg</li>
-            <li>Favourite color: Pink & Light Blue</li>
-            <p>Love: Barfi</p>
-            <p>Important Note: Fat in her family</p>
+            <li>Name : &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Alishba</li>
+            <li>Qualification: &nbsp;&nbsp;&nbsp;&nbsp;BS(Math)</li>
+            <li>Address City: &nbsp;&nbsp;&nbsp;&nbsp;Gujrawala</li>
+            <li>Date of Birth: &nbsp;&nbsp;&nbsp;&nbsp;12 Jan</li>
+            <li>Eye color: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Light brown</li>
+            <li>Hair color: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Brown</li>
+            <li>Height: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5.2`</li>
+            <li>Weight: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;50kg</li>
+            <li>Fav-color: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pink & Light Blue</li>
+            <p>Love: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Barfi</p>
           </ol>
+            <p className="text-[21px] font-bold text-black pt-4">Note:</p>&nbsp;Very Fat in her family
         </div>
       </div>
 
